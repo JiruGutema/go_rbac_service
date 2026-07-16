9d31b0a3086a543ff16061f7bd8d64e564f74553
JiruGutema <jirudagutema@gmail.com>
2026-07-16 09:36:54 +0300

    feat: add configs (laoding .env via godotenv)

--- config/config.go
+++ config/config.go
@@ -1,24 +1,80 @@
+// Package config contains a configuration loader for environment variables
 package config
 
-import "os"
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 type Config struct {
 	Port        string
+	GinMode     string
+	DBHost      string
+	DBPort      string
+	Domain      string
+	DBUser      string
+	DBPassword  string
+	GoEnv       string
+	DBName      string
+	RedisHost   string
+	RedisPort   string
 	DatabaseURL string
-	RedisAddr   string
+	RedisURL    string
 }
 
-func Load() Config {
-	return Config{
-		Port:        getEnv("PORT", "8080"),
-		DatabaseURL: getEnv("DATABASE_URL", "postgres://rbac:rbac@localhost:5432/rbac?sslmode=disable"),
-		RedisAddr:   getEnv("REDIS_ADDR", "localhost:6379"),
+func LoadConfig() *Config {
+	return &Config{
+		Port:        getEnv("PORT", ""),
+		Domain:      getEnv("DOMAIN", ""),
+		GinMode:     getEnv("GIN_MODE ", ""),
+		DBHost:      getEnv("DB_HOST", ""),
+		DBPort:      getEnv("DB_PORT", ""),
+		GoEnv:       getEnv("GO_ENV", ""),
+		DBUser:      getEnv("DB_USER", ""),
+		DBPassword:  getEnv("DB_PASSWORD", ""),
+		DBName:      getEnv("DB_NAME", ""),
+		RedisHost:   getEnv("REDIS_HOST", ""),
+		RedisPort:   getEnv("REDIS_PORT", ""),
+		RedisURL:    getEnv("REDIS_URL", ""),
+		DatabaseURL: getEnv("DATABASE_URL", ""),
 	}
 }
 
-func getEnv(key, fallback string) string {
-	if v := os.Getenv(key); v != "" {
-		return v
+func getEnv(key string, defaultValue string) string {
+	err := godotenv.Load()
+	if err != nil {
+		return defaultValue
 	}
-	return fallback
+	value := os.Getenv(key)
+	if value == "" {
+		return defaultValue
+	}
+
+	return value
+}
+
+func ConstructDBConnectionString(cfg Config) string {
+	if cfg.DatabaseURL != "" {
+		return cfg.DatabaseURL
+	}
+	return fmt.Sprintf(
+		"postgres://%s:%s@%s/%s?sslmode=require",
+		cfg.DBUser,
+		cfg.DBPassword,
+		cfg.DBHost,
+		cfg.DBName,
+	)
+}
+
+func ConstructRedisConnectionString(cfg Config) string {
+	if cfg.RedisURL != "" {
+		return cfg.RedisURL
+	}
+	return fmt.Sprintf(
+		"%s:%s",
+		cfg.RedisHost,
+		cfg.RedisPort,
+	)
 }
